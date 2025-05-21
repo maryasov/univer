4bf431b41d3ee56a619a20477f964ac73cc7dce4
wpxp@123456 <2677556700@qq.com>
2025-05-21 19:36:48 +0800

    feat: optimize the display of numbers that lose precision

--- examples/src/sheets/main.ts
+++ examples/src/sheets/main.ts
@@ -21,7 +21,6 @@ import { UniverDocsPlugin } from '@univerjs/docs';
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui';
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
 import { UniverRenderEnginePlugin } from '@univerjs/engine-render';
-import { DEFAULT_WORKBOOK_DATA_DEMO } from '@univerjs/mockdata';
 import { UniverNetworkPlugin } from '@univerjs/network';
 import { UniverRPCMainThreadPlugin } from '@univerjs/rpc';
 import { UniverSheetsPlugin } from '@univerjs/sheets';
@@ -151,7 +150,7 @@ function createNewInstance() {
 
     // create univer sheet instance
     if (!IS_E2E) {
-        univer.createUnit(UniverInstanceType.UNIVER_SHEET, DEFAULT_WORKBOOK_DATA_DEMO);
+        univer.createUnit(UniverInstanceType.UNIVER_SHEET, {});
     }
 
     setTimeout(() => {
