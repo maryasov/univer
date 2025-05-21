1bdafd81c5b700bb70a8933bada3a2722fe413da
wpxp@123456 <2677556700@qq.com>
2025-05-21 17:11:18 +0800

    feat: optimize the display of numbers that lose precision

--- examples/src/sheets/main.ts
+++ examples/src/sheets/main.ts
@@ -112,7 +112,9 @@ function createNewInstance() {
         notExecuteFormula: true,
         autoHeightForMergedCells: true,
     });
-    univer.registerPlugin(UniverSheetsUIPlugin);
+    univer.registerPlugin(UniverSheetsUIPlugin, {
+        disableForceStringAlert: true,
+    });
     univer.registerPlugin(UniverSheetsNumfmtPlugin, {
         disableTextFormatAlert: true,
         disableTextFormatMark: true,
